cbebd500cb11504ba111dfb052532ac255b8d7ee
Jackson Loper <jloper@Jacksons-MacBook-Pro.local>
2020-04-07 16:50:16 -0400

    I'm a real package now

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='leggps',
+    author='Jackson Loper',
+    version='0.0.1',
+    description='Tools for GPs on one dimension',
+    packages=['leggps'],
+)
